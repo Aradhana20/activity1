27749cbd8da4fd897f6193dd3d6bb0e874a209c9
Aradhana20 <56786845+Aradhana20@users.noreply.github.com>
2020-06-01 15:56:55 +0530

    Update file.c

--- file.c
+++ file.c
@@ -4,10 +4,10 @@
 int main()
 {
 
-    int a, b, c, big;
+    int a, b, cd, big;
     printf("Enter the 3 numbers:");
-    scanf("%d %d %d", &a, &b, &c);
-    big =  a>b ?(a>c ? a : c):(b>c ? b : c);
+    scanf("%d %d %d", &a, &b, &cd);
+    big =  a>b ?(a>cd ? a : cd):(b>cd ? b : cd);
     printf("the biggest number is : %d", big);
     return 0;
 }
